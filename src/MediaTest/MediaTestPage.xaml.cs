bc0eec7eacab813e767786a6299b4b9f6ad0d576
Bartlomiej Pater <bartlomiej.pater@thinkproject.com>
2017-03-17 12:00:37 +0100

    test app again

--- src/MediaTest/MediaTestPage.xaml.cs
+++ src/MediaTest/MediaTestPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Reflection;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
 
 namespace MediaTest
 {
@@ -7,6 +11,40 @@ namespace MediaTest
         public MediaTestPage()
         {
             InitializeComponent();
+            CrossMedia.Current.Initialize();
         }
+
+        public async void TakePhoto(object sender, EventArgs args)
+        {
+            var photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+            {
+                Name = "photo.jpg",
+                PhotoSize = PhotoSize.Full,
+                SaveToAlbum = false
+            });
+            var stream = photo.GetStream();
+            ExaminePhoto(stream);
+        }
+
+        public async void PickPhoto(object sender, EventArgs args)
+        {
+            var photo = await CrossMedia.Current.PickPhotoAsync();
+            var stream = photo.GetStream();
+            ExaminePhoto(stream);
+        }
+
+        private void ExaminePhoto(System.IO.Stream stream)
+        {
+            var exif = ExifLib.ExifReader.ReadJpeg(stream);
+            string exif_string = $"Dimenssions: {exif.Width}x{exif.Height}\n" +
+                                 $"Date taken: {exif.DateTime}\n" +
+                                 $"Camera model: {exif.Model}\n" +
+                                 $"Camera maker: {exif.Make}\n" +
+                                 $"Orientation: {exif.Orientation}\n" +
+                                 $"GPS Lat: {exif.GpsLatitude[0]}'{exif.GpsLatitude[1]}\"{exif.GpsLatitude[2]}\n" +
+                                 $"GPS Long: {exif.GpsLongitude[0]}'{exif.GpsLongitude[1]}\"{ exif.GpsLongitude[2]}";
+            DisplayAlert("photo", exif_string, "ok");
+        }
+
     }
 }
