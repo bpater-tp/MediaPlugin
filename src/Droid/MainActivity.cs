ae9c6428e6fed81f269d7d35a8d5813f3d8db027
Bartlomiej Pater <bartlomiej.pater@thinkproject.com>
2017-03-23 11:19:39 +0100

    demo app updated for location trancking

--- src/Droid/MainActivity.cs
+++ src/Droid/MainActivity.cs
@@ -7,12 +7,16 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Locations;
 
 namespace MediaTest.Droid
 {
     [Activity(Label = "MediaTest.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, ILocationListener
     {
+        private LocationManager _locationManager;
+        private string _provider;
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -22,7 +26,60 @@ namespace MediaTest.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
+            _locationManager = GetSystemService(Context.LocationService) as LocationManager;
             LoadApplication(new App());
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            _provider = LocationManager.GpsProvider;
+            if (_locationManager.IsProviderEnabled(_provider))
+            {
+                App.AreLocationServicesEnabled = true;
+                _locationManager.RequestLocationUpdates(_provider, 3000, 10, this);
+            }
+            else
+            {
+                Console.WriteLine("No location services enabled");
+            }
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _locationManager.RemoveUpdates(this);
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            if (provider == _provider)
+            {
+                App.AreLocationServicesEnabled = true;
+            }
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            if (provider == _provider)
+            {
+                App.AreLocationServicesEnabled = false;
+            }
+        }
+        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        {
+            
+        }
+        public void OnLocationChanged(Location location)
+        {
+            App.Location.Altitude = location.Altitude;
+            App.Location.Latitude = location.Latitude;
+            App.Location.Longitude = location.Longitude;
+            App.Location.Speed = location.Speed;
+            App.Location.HorizontalAccuracy = location.Accuracy;
+            App.Location.Direction = location.Bearing;
+            App.Location.Timestamp = new DateTime(location.Time);
         }
     }
 }
