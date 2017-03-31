81715b4ba9028ad813d3476ca351d0b097a6a195
Bartlomiej Pater <bartlomiej.pater@thinkproject.com>
2017-03-31 12:17:07 +0200

    Remove one Task.Run

--- src/Media.Plugin.iOS/PhotoLibraryAccess.cs
+++ src/Media.Plugin.iOS/PhotoLibraryAccess.cs
@@ -21,30 +21,27 @@ namespace Plugin.Media
             var image = PHAsset.FetchAssets(urls, new PHFetchOptions()).firstObject as PHAsset;
             await Task.Run(() =>
             {
-                Task.Run(() =>
+                image.RequestContentEditingInput(options, (contentEditingInput, requestStatusInfo) =>
                 {
-                    image.RequestContentEditingInput(options, (contentEditingInput, requestStatusInfo) =>
+                    try
                     {
-                        try
-                        {
-                           var fullimage = CIImage.FromUrl(contentEditingInput.FullSizeImageUrl);
-                           if (fullimage?.Properties != null)
-                           {
-                                meta = new NSMutableDictionary();
-                                meta[ImageIO.CGImageProperties.Orientation] = new NSString(fullimage.Properties.Orientation.ToString());
-                                meta[ImageIO.CGImageProperties.ExifDictionary] = fullimage.Properties.Exif?.Dictionary;
-                                meta[ImageIO.CGImageProperties.TIFFDictionary] = fullimage.Properties.Tiff?.Dictionary;
-                                meta[ImageIO.CGImageProperties.GPSDictionary] = fullimage.Properties.Gps?.Dictionary;
-                                meta[ImageIO.CGImageProperties.IPTCDictionary] = fullimage.Properties.Iptc?.Dictionary;
-                                meta[ImageIO.CGImageProperties.JFIFDictionary] = fullimage.Properties.Jfif?.Dictionary;
-                           }
-                           completed = true;
-                        }
-                        catch (Exception ex)
-                        {
-                           completed = true;
-                        }
-                    });
+                       var fullimage = CIImage.FromUrl(contentEditingInput.FullSizeImageUrl);
+                       if (fullimage?.Properties != null)
+                       {
+                            meta = new NSMutableDictionary();
+                            meta[ImageIO.CGImageProperties.Orientation] = new NSString(fullimage.Properties.Orientation.ToString());
+                            meta[ImageIO.CGImageProperties.ExifDictionary] = fullimage.Properties.Exif?.Dictionary;
+                            meta[ImageIO.CGImageProperties.TIFFDictionary] = fullimage.Properties.Tiff?.Dictionary;
+                            meta[ImageIO.CGImageProperties.GPSDictionary] = fullimage.Properties.Gps?.Dictionary;
+                            meta[ImageIO.CGImageProperties.IPTCDictionary] = fullimage.Properties.Iptc?.Dictionary;
+                            meta[ImageIO.CGImageProperties.JFIFDictionary] = fullimage.Properties.Jfif?.Dictionary;
+                       }
+                       completed = true;
+                    }
+                    catch (Exception ex)
+                    {
+                       completed = true;
+                    }
                 });
                 while (!completed)
                 {
