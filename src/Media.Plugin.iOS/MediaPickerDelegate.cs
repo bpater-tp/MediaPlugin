80c2c2a6b508d6c6c3530e1fd81d42ca76fcae33
Bartlomiej Pater <bartlomiej.pater@thinkproject.com>
2020-08-06 10:57:16 +0200

    fix nullable gps coords

--- src/Media.Plugin.iOS/MediaPickerDelegate.cs
+++ src/Media.Plugin.iOS/MediaPickerDelegate.cs
@@ -449,10 +449,16 @@ namespace Plugin.Media
             var newMeta = new NSMutableDictionary();
             newMeta.SetValuesForKeysWithDictionary(meta);
             var newGpsDict = new NSMutableDictionary();
-            newGpsDict.SetValueForKey(new NSNumber(Math.Abs(location.Latitude)), ImageIO.CGImageProperties.GPSLatitude);
-            newGpsDict.SetValueForKey(new NSString(location.Latitude > 0 ? "N" : "S"), ImageIO.CGImageProperties.GPSLatitudeRef);
-            newGpsDict.SetValueForKey(new NSNumber(Math.Abs(location.Longitude)), ImageIO.CGImageProperties.GPSLongitude);
-            newGpsDict.SetValueForKey(new NSString(location.Longitude > 0 ? "E" : "W"), ImageIO.CGImageProperties.GPSLongitudeRef);
+            if (location?.Latitude != null)
+			{
+                newGpsDict.SetValueForKey(new NSNumber(Math.Abs((double)location.Latitude)), ImageIO.CGImageProperties.GPSLatitude);
+                newGpsDict.SetValueForKey(new NSString(location.Latitude > 0 ? "N" : "S"), ImageIO.CGImageProperties.GPSLatitudeRef);
+            }
+            if (location?.Longitude != null)
+			{
+                newGpsDict.SetValueForKey(new NSNumber(Math.Abs((double)location.Longitude)), ImageIO.CGImageProperties.GPSLongitude);
+                newGpsDict.SetValueForKey(new NSString(location.Longitude > 0 ? "E" : "W"), ImageIO.CGImageProperties.GPSLongitudeRef);
+            }
             newGpsDict.SetValueForKey(new NSNumber(location.Altitude), ImageIO.CGImageProperties.GPSAltitude);
             newGpsDict.SetValueForKey(new NSNumber(0), ImageIO.CGImageProperties.GPSAltitudeRef);
             newGpsDict.SetValueForKey(new NSNumber(location.Speed), ImageIO.CGImageProperties.GPSSpeed);
